12878341a06bc74cdbecc916528c46a539f6cf2e
Christoph Timm <quaquaro@gmail.com>
2022-02-14 19:36:48 +0100

    Add component Balance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Header } from "./components/Header.js";
+import { Balance } from "./components/Balance.js";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
