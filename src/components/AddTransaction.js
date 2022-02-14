5c9285be4c02816a356d6060f2f17c0ca4e37fce
Christoph Timm <quaquaro@gmail.com>
2022-02-14 21:51:24 +0100

    Add the rest of fuctionality

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState.js";
+import { nanoid } from "nanoid";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: nanoid(),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Füge eine neue Transaktion hinzu</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Gebe Text ein..." />
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState.js";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Einkommen</h4>
         <p id="money-plus" className="money plus">
-          +0.00 €
+          {income} €
         </p>
       </div>
       <div>
         <h4>Ausgaben</h4>
         <p id="money-minus" className="money minus">
-          -0.00 €
+          {expense} €
         </p>
       </div>
     </div>
